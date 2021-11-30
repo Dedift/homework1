fd46525f0794466b9a5580484a6a7ec0cc95af6b
Dedift <staszhvansky@gmail.com>
2021-11-30 19:12:47 +0300

    Multithreading

---
+++ src/com/company/homeworks/homework11/Threads/FactoryThread.java
@@ -0,0 +1,20 @@
+package com.company.homeworks.homework11.Threads;
+
+import com.company.homeworks.homework11.Dump;
+
+public class FactoryThread extends Thread {
+
+    public static final int COUNT_NIGHTS = 100;
+    private Dump dump;
+
+    public FactoryThread(Dump dump) {
+        this.dump = dump;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < COUNT_NIGHTS; i++) {
+            dump.addDetails();
+        }
+    }
+}
