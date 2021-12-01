c884bf4520f951df5fdb5773fa1d8537a3c980dc
Dedift <staszhvansky@gmail.com>
2021-12-01 23:05:52 +0300

    Multithreading

---
+++ src/com/company/homeworks/homework11/Detail.java
@@ -0,0 +1,38 @@
+package com.company.homeworks.homework11;
+
+public class Detail {
+
+    private String name;
+    private final String[] details = {"Head", "CPU", "RAM", "HDD", "LeftHand", "RightHand", "LeftLeg", "RightLeg"};
+
+    public Detail() {
+        int indexName = (int) (Math.random() * (details.length));
+        this.name = details[indexName];
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Detail detail = (Detail) o;
+
+        return name != null ? name.equals(detail.name) : detail.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Details{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+}
